c77dae423c5fa3f667ff40637b8129c9ebc9e0e5
tiago-sb <belatiago72@gmail.com>
2023-09-26 18:49:54 -0300

    adiciona o primeiro código ao repositório

---
+++ apII_unidade01/lista_I/src/primeiros_passos/Exemplo_I.java
@@ -0,0 +1,16 @@
+/*
+ * Exercício 1: Soma de Dois Números
+ * Solicite ao usuário dois números e exiba a soma deles.
+ */
+package primeiros_passos;
+import java.util.Scanner;
+public class Exemplo_I {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int primeiroValor = entrada.nextInt();
+		int segundoValor = entrada.nextInt();
+		int resultado = primeiroValor + segundoValor;
+		System.out.print(resultado);
+		entrada.close();;
+	}
+}
