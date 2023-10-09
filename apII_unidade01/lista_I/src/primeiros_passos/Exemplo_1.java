1d670a6f97507617a76fa40c33b0638ca3a4944f
tiago-sb <belatiago72@gmail.com>
2023-10-09 20:46:58 -0300

    modifica o nome da classe principal da primeira questão

--- apII_unidade01/lista_I/src/primeiros_passos/Exemplo_1.java
+++ apII_unidade01/lista_I/src/primeiros_passos/Exemplo_1.java
@@ -1,10 +1,9 @@
-/*
- * Exercício 1: Soma de Dois Números
- * Solicite ao usuário dois números e exiba a soma deles.
- */
+// Exercício 1: Soma de Dois Números
+// Solicite ao usuário dois números e exiba a soma deles.
+ 
 package primeiros_passos;
 import java.util.Scanner;
-public class Exemplo_I {
+public class Exemplo_1 {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		int primeiroValor = entrada.nextInt();
