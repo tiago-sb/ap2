b7aa679803158606731451ea6274a936a4b71aa4
tiago-sb <belatiago72@gmail.com>
2023-10-10 18:32:25 -0300

    adiciona o código do Exemplo_3

--- apII_unidade01/lista_I/src/primeiros_passos/Exemplo_3.java
+++ apII_unidade01/lista_I/src/primeiros_passos/Exemplo_3.java
@@ -1,10 +1,15 @@
+// Conversão de Temperatura
+// Peça ao usuário para inserir uma temperatura em graus Celsius e converta-a para
+// Fahrenheit. Exiba o resultado.
+
 package primeiros_passos;
-
+import java.util.Scanner;
 public class Exemplo_3 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner entrada = new Scanner(System.in);
+		float temperaturaCelsius = entrada.nextFloat();
+		float temperaturaFahrenheit = (temperaturaCelsius * 1.8F) + 32;
+		System.out.println(temperaturaFahrenheit);
+		entrada.close();
 	}
-
 }
