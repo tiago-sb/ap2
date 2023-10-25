b782d23b2c1bb3344b30b4fe727e43de92dafc87
tiago-sb <belatiago72@gmail.com>
2023-10-25 17:26:42 -0300

    modificações ao código do exemplo 5, classe main

--- apII_unidade02/src/lista6/exemplo05/Main.java
+++ apII_unidade02/src/lista6/exemplo05/Main.java
@@ -6,11 +6,13 @@ package lista6.exemplo05;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
+		// instancias de classes que serão úteis
 		Scanner entradaDeDados = new Scanner(System.in);
 		Palindromo pl = new Palindromo();
-		
+
 		String palavraEscolhida = entradaDeDados.nextLine();
 		
+		// caso a palavra escolhida for um palíndromo: true
 		System.out.println(pl.palindromo(palavraEscolhida));
 		
 		entradaDeDados.close();
