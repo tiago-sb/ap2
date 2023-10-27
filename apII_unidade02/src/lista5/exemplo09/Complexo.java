689f4b23d098a35d3a020943be263eb127f7c354
tiago-sb <belatiago72@gmail.com>
2023-10-27 00:03:46 -0300

    adiciona o código da questão 09 ao versionamento

--- apII_unidade02/src/lista5/exemplo09/Complexo.java
+++ apII_unidade02/src/lista5/exemplo09/Complexo.java
@@ -1,4 +1,50 @@
 package lista5.exemplo09;
 public class Complexo {
+	// atributos
+	private double real;
+	private double imaginaria;
+	// metodos
+	//construtores
+	public Complexo(double real, double imaginaria) {
+		this.real = real;
+		this.imaginaria = imaginaria;
+	}
+	public Complexo() {}
+	// get e set
+	public double getReal() {
+		return real;
+	}
+	public void setReal(double real) {
+		this.real = real;
+	}
+	public double getImaginaria() {
+		return imaginaria;
+	}
+	public void setImaginaria(double imaginaria) {
+		this.imaginaria = imaginaria;
+	}
 	
+	// soma
+	// soma e retorna um objeto da classe Complexo com os parametros atualizados
+	public Complexo somarComplexo(Complexo comp) {
+		double parteReal = real + comp.real;
+		double parteImaginaria = imaginaria + comp.imaginaria;
+		return new Complexo(parteReal, parteImaginaria);
+	}
+	
+	// uma nova cara para a função equals do java
+	@Override
+	public boolean equals(Object comp1) {
+		if((comp1 != null) && (comp1 instanceof Complexo)) {
+			Complexo comp2 = (Complexo) comp1;
+			return (comp2.real == real && comp2.imaginaria == imaginaria);
+		}
+		
+		return false;
+	}
+	// imprimir o número real na tela
+	@Override
+	public String toString() {
+		return real + " + " + imaginaria + "i"; 
+	}
 }
--- apII_unidade02/src/lista5/exemplo09/Main.java
+++ apII_unidade02/src/lista5/exemplo09/Main.java
@@ -5,6 +5,25 @@
 package lista5.exemplo09;
 public class Main {
 	public static void main(String[] args) {
+		Complexo primeiroCompleto = new Complexo(12, 6);
+		Complexo segundoCompleto = new Complexo(17, 6.2);
+		Complexo terceiroCompleto = new Complexo(15.6, 6);
+		Complexo quartoCompleto = new Complexo(19, 6);
+		Complexo quintoCompleto = new Complexo(12, 6);
+		
+		//imprimir
+		System.out.println(primeiroCompleto.toString());
+		// soma
+		Complexo soma = primeiroCompleto.somarComplexo(segundoCompleto);
+		System.out.println(soma.toString());
 
+		// comparação
+		if(primeiroCompleto.equals(quintoCompleto)) {
+			System.out.println("iguais!");
+		}
+		else {
+			System.out.println("diferentes!");
+		}
 	}
 }
+// c.equals(c1)
