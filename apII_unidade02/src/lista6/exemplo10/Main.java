af4d391312a629992c43bb8b265f5ddd8711d842
tiago-sb <belatiago72@gmail.com>
2023-10-30 19:58:52 -0300

    adiciona o código do exemplo 10 ao versionamento|

--- apII_unidade02/src/lista6/exemplo10/Main.java
+++ apII_unidade02/src/lista6/exemplo10/Main.java
@@ -5,6 +5,14 @@ package lista6.exemplo10;
 
 public class Main {
 	public static void main(String[] args) {
-		int[] vetorInteiros = {};
+		// instancia do objeto
+		MaiorElemento array = new MaiorElemento();
+
+		// declaração das variáveis multiplas
+		int[] vetorInteiros = {2, 8, 98, 75, 42, 4, 21, 8, 3, 15};
+		int maior = vetorInteiros[0];
+
+		// impressão
+		System.out.println(array.maiorElemento(vetorInteiros, vetorInteiros.length, maior));
 	}
 }
--- apII_unidade02/src/lista6/exemplo10/MaiorElemento.java
+++ apII_unidade02/src/lista6/exemplo10/MaiorElemento.java
@@ -1,5 +1,15 @@
 package lista6.exemplo10;
 
 public class MaiorElemento {
-
-}
+	public int maiorElemento(int[] vetorInteiros, int posicao, int maior) {
+		if(posicao <= 0) {
+			return maior;
+		} else {
+			if(maior < vetorInteiros[posicao - 1]) {
+				maior = vetorInteiros[posicao - 1];
+			}
+			
+			return maiorElemento(vetorInteiros, posicao - 1, maior);
+		}
+	}
+}	
